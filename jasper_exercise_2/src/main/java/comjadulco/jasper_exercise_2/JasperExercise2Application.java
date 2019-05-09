9b2c0c56e0acc6b018e5b77b1fe37b5b2ece6642
sushmitajadulco <sushmitajadulco116@gmail.com>
2019-05-10 04:30:29 +0800

    added jasper report exercise

---
+++ jasper_exercise_2/src/main/java/comjadulco/jasper_exercise_2/JasperExercise2Application.java
@@ -0,0 +1,13 @@
+package comjadulco.jasper_exercise_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperExercise2Application {
+
+	public static void main(String[] args) {
+			SpringApplication.run(JasperExercise2Application.class, args);
+	}
+
+}
---
+++ jasper_exercise_2/src/test/java/comjadulco/jasper_exercise_2/JasperExercise2ApplicationTests.java
@@ -0,0 +1,16 @@
+package comjadulco.jasper_exercise_2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JasperExercise2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
